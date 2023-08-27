231936cd5c92b20b6242aa45cb4c691551409d17
Alihashmi7 <137565752+Alihashmi7@users.noreply.github.com>
2023-08-27 05:38:27 -0800

    Update Ascript.js

--- java/Ascript.js
+++ java/Ascript.js
@@ -36,3 +36,64 @@ document.addEventListener("DOMContentLoaded", function() {
         confirmationSection.style.display = "block";
     });
 });
+
+// Add Firebase configuration (replace with your actual configuration)
+const firebaseConfig = {
+    apiKey: "AIzaSyBhN5xNiLKgrZNF1N1agfISDK9mzNp1eDI",
+    authDomain: "healthholicspk.firebaseapp.com",
+    databaseURL: "https://healthholicspk-default-rtdb.firebaseio.com",
+    projectId: "healthholicspk",
+    storageBucket: "healthholicspk.appspot.com",
+    messagingSenderId: "956815724673",
+    appId: "1:956815724673:web:bd6ff8bc87e5690ddefd80",
+    measurementId: "G-62FYLL4Y9T"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get references to Firebase database and messaging
+const database = firebase.database();
+const messaging = firebase.messaging();
+
+// ... (existing code)
+
+bookingForm.addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+
+    const bookedAppointmentsRef = database.ref(`appointments/${date}`);
+
+    // Retrieve existing appointments for the selected date
+    const snapshot = await bookedAppointmentsRef.once("value");
+    const bookedAppointments = snapshot.val() || [];
+
+    if (bookedAppointments.length >= 2) {
+        alert("Maximum appointments for this date have been reached.");
+        return;
+    }
+
+    // Add the new appointment to the list for the selected date
+    bookedAppointments.push({ name, email, time });
+    bookedAppointmentsRef.set(bookedAppointments);
+
+    // Send a notification to the nutritionist
+    const notificationMessage = `${name} has booked an appointment on ${date} at ${time}.`;
+    const notificationPayload = {
+        notification: {
+            title: "New Appointment",
+            body: notificationMessage
+        }
+    };
+
+    const nutritionistToken = "YOUR_NUTRITIONIST_FCM_TOKEN"; // Replace with the actual token
+
+    await messaging.sendToDevice(nutritionistToken, notificationPayload);
+
+    // ... (rest of your code)
+});
+
